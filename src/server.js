b0bf8b46f3fcdc21168d5499376cd9fd54ac1049
Gabriel <gabriel.diasdutra2@gmail.com>
2020-09-26 22:18:45 -0300

    feat: Modificando server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
 
 const express = require('express')
 const cors = require('cors')
 
 const Routes = require('./routes/Routes')
+
 const server = express()
 
 server.use(express.json())
@@ -11,6 +14,14 @@ server.use(cors())
 
 server.use('/v1', Routes)
 
-server.listen(process.env.PORT, () => {
-  console.log(`[Server] Server started on port: ${process.env.PORT}`)
-})
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+} else {
+  server.listen(process.env.PORT, () => {
+    console.log(
+      `[Server] [Cluster: ${cluster.worker.id}] Server started on port: ${process.env.PORT}`,
+    )
+  })
+}
